ee986ba926f9c7ff10ba6192f3be110fabc52334
Victor Silva <victorhfsilva@protonmail.com>
2025-06-15 23:18:22 -0300

    feat: add sensitive files enum

---
+++ sensitive-files-enum.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+user="$(whoami)"
+host="$(hostname)"
+date="$(date -I)"
+filename="sensitive_files_enum_${user}_${host}_${date}.txt"
+words_wordlist="sensitive-words-wordlist.txt"
+extensions_wordlist="sensitive-extensions-wordlist.txt"
+
+# Files with sensitive words in their names
+printf "Files with Sensitive Words in Their Names:\n" >> "${filename}"
+find / -type f \( -iname "*pass*" -o -iname "*token*" -o -iname "*secret*" -o -iname "*key*" -o -iname "*senha*" \
+    -o -iname "*user*" -o -iname "*cred*" -o -iname "*config*" -o -iname "*login*" -o -iname "*db*" -o -iname "*usuario*" \) \
+    2>/dev/null >> "${filename}"
+printf "\n" >> "${filename}"
+
+# History files with sensitive commands
+printf "History Files:\n" >> "${filename}"
+find /home /root -type f \( -name '*_hist' -o -name '*_history' \) \
+    -exec sh -c '
+        file="$1"
+        wordlist="$2"
+        echo -e "\n[+] $(ls -al "$file")"
+        echo -e "\nHistory Sensitive Commands:"
+        grep -Eif "$wordlist" "$file" 2>/dev/null || echo "(nenhuma correspondência encontrada)"
+    ' _ {} "$(realpath "$words_wordlist")" \; 2>/dev/null >> "${filename}"
+printf "\n" >> "${filename}"
+
+# Files with sensitive extensions
+printf "Files with Sensitive Extensions:\n" >> "${filename}"
+expr=""
+while read -r ext; do
+    ext="${ext#.}"  # remove ponto inicial se existir
+    expr="$expr -o -iname '*.$ext'"
+done < "$extensions_wordlist"
+expr="${expr# -o }"
+eval "find / -type f \( $expr \) 2>/dev/null" >> "${filename}"
+printf "\n" >> "${filename}"
+
+# Files with sensitive words in their content
+printf "Files with Sensitive Words in Their Content:\n" >> "${filename}"
+find / -type f -readable -size -5M 2>/dev/null \
+    -exec sh -c '
+        wordlist="$1"
+        file="$2"
+        grep -Eif "$wordlist" "$file" 2>/dev/null && echo -e "\n[+] Arquivo: $file\n"
+    ' _ "$(realpath "$words_wordlist")" {} \; >> "${filename}"
+printf "\n" >> "${filename}"
+
+# Hidden files and directories
+printf "Hidden Files and Directories:\n" >> "${filename}"
+find / \( -type f -name '.*' -o -type d -name '.*' \) -exec ls -adl {} \; 2>/dev/null >> "${filename}"
+printf "\n" >> "${filename}"
+
+# Sensitive logs
+printf "Sensitive Logs:\n" >> "${filename}"
+for file in /var/log/* 2>/dev/null; do
+    matches=$(grep -Ei "accepted|session opened|session closed|failure|failed|ssh|password changed|new user|delete user|sudo|COMMAND=|logs" "$file" 2>/dev/null)
+    if [[ -n "$matches" ]]; then
+        echo -e "\nLog file: $file"
+        echo "$matches"
+    fi
+done >> "${filename}"
+printf "\n" >> "${filename}"
+
+base64 -w 0 ${filename} >> "${filename}_encoded"
+
+# Print completion message
+printf "Sensitive files enumeration completed. Output saved to %s\n" "${filename}"
